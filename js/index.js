4e5e9dbf206d2df128417173c454764dbf3e7940
dailiwang <758017188@qq.com>
2017-04-03 17:56:38 +0800

    modify some bug

--- js/index.js
+++ js/index.js
@@ -237,7 +237,7 @@ $(function(){
 			$(".kuang").css("display","none");
 			$("#confirm").css("display","none");
 			$("#confirm").unbind();
-		},300);
+		},500);
 	});
 
 	//得到专业的value值
@@ -342,21 +342,21 @@ $(function(){
 			"tea_Name": temp.tea_Name
 		};
 		postComment(postdata,function(isComment,err){
-			/*if(err){
+			if(err){
 				showbox("网络错误");
 			}else{
 				isComment = Number.parseInt(isComment)
 				if(isComment == 0){
 					showbox("您本月度对此老师评论过了！");
-				}else{*/
+				}else{
 					//向上滑动到下一页
 					if (isAnimating) return;
 					last.row = now.row;
 					last.col = now.col;
 					if (last.row != 12) { now.row = last.row+1; now.col = 1; pageMove(towards.up);}
 					return true;
-				/*}
-			}*/
+				}
+			}
 		});
 	}
 	
